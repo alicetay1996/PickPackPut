c068a6f2f92e6d042419e5d5c40aac40e07b1464
AliceTay1996 <alice.tay@pingspace.co>
2021-01-20 21:32:08 +0800

    New Development

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { BoundDirectivePropertyAst } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { BodyComponent } from './body/body.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'application';
+  constructor(){}
 }
